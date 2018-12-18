19ba32e175486ecac583eab810a888a33bd91350
maximren <renoa325@gmail.com>
2018-12-18 22:38:31 +0200

    Updates

---
+++ precache-manifest.a2d77415b669581e2e7affd77a54ee08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f680c5878b7659889591",
+    "url": "/todos/static/js/runtime~main.f680c587.js"
+  },
+  {
+    "revision": "dbcafc166d190245101b",
+    "url": "/todos/static/js/main.dbcafc16.chunk.js"
+  },
+  {
+    "revision": "ae56898e97de8327980d",
+    "url": "/todos/static/js/1.ae56898e.chunk.js"
+  },
+  {
+    "revision": "dbcafc166d190245101b",
+    "url": "/todos/static/css/main.8d25ba4c.chunk.css"
+  },
+  {
+    "revision": "9a398447e502106651079e7f4149111f",
+    "url": "/todos/index.html"
+  }
+];
