f472f5fd32ea4bbde13ab43660741f14a806126d
maximren <renoa325@gmail.com>
2018-12-18 23:39:19 +0200

    add history module

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Work from './components/Work';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import { createBrowserHistory } from 'history';
+import { createBrowserHistory } from 'history/createBrowserHistory';
 
 const history = createBrowserHistory({
   basename: process.env.PUBLIC_URL,
