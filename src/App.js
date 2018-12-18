d01b0b76c879eb06fe425630a89108157521f4d5
maximren <renoa325@gmail.com>
2018-12-18 23:36:58 +0200

    add history module

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ import Work from './components/Work';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import createHistory from 'history';
+import { createBrowserHistory } from 'history';
 
-const history = createHistory({
+const history = createBrowserHistory({
   basename: process.env.PUBLIC_URL,
 });
 
 class App extends PureComponent {
   render() {
     return (
-        <Provider store={store}>
-          <BrowserRouter history={history}>
-            <div className="App">
-              <Navbar />
-                <Route exact path="/" component={Own} />
-                <Route path="/education" component={Education} />
-                <Route path="/work" component={Work} />
-            </div>
-          </BrowserRouter>
-        </Provider>
+      <Provider store={store}>
+        <BrowserRouter history={history}>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Own} />
+            <Route path="/education" component={Education} />
+            <Route path="/work" component={Work} />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
