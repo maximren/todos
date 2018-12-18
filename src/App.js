d011499f26ef1a4977ab10b83fb84e7b29ecdef8
maximren <renoa325@gmail.com>
2018-12-18 23:27:37 +0200

    fixed reload error

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@ import Work from './components/Work';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import { createHistory } from 'history';
+
+const history = createHistory({
+  basename: process.env.PUBLIC_URL,
+});
 
 class App extends PureComponent {
   render() {
     return (
         <Provider store={store}>
-          <BrowserRouter>
+          <BrowserRouter history={history}>
             <div className="App">
               <Navbar />
-                <Route exact path="todos/" component={Own} />
-                <Route path="todos/education" component={Education} />
-                <Route path="todos/work" component={Work} />
+                <Route exact path="/" component={Own} />
+                <Route path="/education" component={Education} />
+                <Route path="/work" component={Work} />
             </div>
           </BrowserRouter>
         </Provider>
