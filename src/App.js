89141bbeb11698441cfc41f0b3cf4d921e5367ce
maximren <renoa325@gmail.com>
2018-12-18 23:07:33 +0200

    using hash router for gh-pages

--- src/App.js
+++ src/App.js
@@ -4,24 +4,23 @@ import Navbar from './components/Navbar';
 import Own from './components/Own';
 import Education from './components/Education';
 import Work from './components/Work';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-
 
 class App extends PureComponent {
   render() {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div className="App">
-            <Navbar />
-            <Route exact path="/" component={Own} />
-            <Route path="/education" component={Education} />
-            <Route path="/work" component={Work} />
-          </div>
-        </BrowserRouter>
-      </Provider>
+        <Provider store={store}>
+          <HashRouter>
+            <div className="App">
+              <Navbar />
+                <Route exact path="/" component={Own} />
+                <Route path="/education" component={Education} />
+                <Route path="/work" component={Work} />
+            </div>
+          </HashRouter>
+        </Provider>
     );
   }
 }
